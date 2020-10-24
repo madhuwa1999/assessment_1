0cbad22ad102256cdc9d3f674f1f80ef7ae71cb1
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2020-10-24 20:57:03 +0530

    removed login added survey

--- res/main.js
+++ res/main.js
@@ -8,26 +8,27 @@ function validate()
 	var letters = /^[A-Za-z]+$/;
 	var fname = document.getElementById("fname").value;
 	var lname = document.getElementById("lname").value;
+	var vname = document.getElementById("vname").value;
 
 	//var readMe = fs.readFileSync('../res/data.txt', 'utf8');
 	//alert(readMe);
 
-	if((fname == "" || fname == null) || (lname == "" || lname == null))
+	if((fname == "" || fname == null) || (lname == "" || lname == null) || (vname == "" || vname == null))
 	{
-		alert("Enter all the text fields. Login Failed.");
+		alert("Enter all the text fields. Survey Failed.");
 	}
 
 	else
 	{
-		if(fname.match(letters) && lname.match(letters))
+		if(fname.match(letters) && lname.match(letters) && vname.match(letters))
 		{
-			alert("Welcome "+fname+" "+lname+". Login Successful.");
+			alert("Welcome "+fname+" "+lname+". Survey Successful.");
 			window.open("../codes/index.html");
 			return true;
 		}
 		else
 		{
-			alert("You can enter charactors only. Login Failed.");
+			alert("You can enter charactors only. Survey Failed.");
 			return false;
 		}
 	}
